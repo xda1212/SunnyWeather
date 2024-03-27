dad5780cefb39666bd78305f758fa3f3f1ac397a
xiaodaan <xiaodaan@wycn.com>
2024-03-27 15:07:54 +0800

    First commit.

---
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sunnyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private val  token :String = "ln0liEHdFQjv2zGp"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
